cee31d741ee3f91fda376915bb6e68482c2c3a3a
vit3k <vit3ktsk@gmail.com>
2019-12-16 03:19:52 +0700

    0,1

---
+++ Chat/Model/Net/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Model
+{
+    [Serializable]
+    public class Message
+    {
+        public UserInfo Sender { get; set; }
+        public UserInfo Recipient { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ Chat/Model/Net/TypeOfData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Model
+{
+    [Serializable]
+    public enum TypeOfData
+    {
+        Connect,
+        Disconnect,
+        Message,
+        UserList,
+        OK,
+        ERROR
+    }
+}
